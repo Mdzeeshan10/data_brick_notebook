3f899e74cf4da7bd3f4949c01d8ef1ec57e38778
Möhämmëd Zëëshän <121537023+Mdzeeshan10@users.noreply.github.com>
2023-02-27 11:25:57 +0530

    first commit data brick notebook

---
+++ notebooks/Users/mohammed.zeeshan@hanu.com/nootbook1.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+dbutils.fs.mount(
+  source = "wasbs://row@zee.blob.core.windows.net",
+  mount_point = "/mnt/zee1",
+  extra_configs = {"fs.azure.account.key.zee.blob.core.windows.net":"YlBjJI7gIJpTUQ5ELXbqiqI/T/WhLRDi03zTBEiVU30O0O9zXwkWqKULPst5k+5cors7NYTbyw/v+AStJ3bIBw=="})
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+import pandas as pd
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
